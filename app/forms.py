1fca9dfd793c12a1576e4eb7dbe77a6051e4ce6c
diane-mahoro <www.diammah7@gmail.com>
2019-09-20 09:36:09 +0200

    login and signup form

--- app/forms.py
+++ app/forms.py
@@ -2,12 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
-# class ReviewForm(FlaskForm):
-
-#     title = StringField('Review title',validators=[Required()])
-#     review = TextAreaField('Movie review', validators=[Required()])
-#     submit = SubmitField('Submit')
-
 class PitchForm(FlaskForm):
 
     category = StringField('Review title',validators=[Required()])
--- app/views.py
+++ app/views.py
@@ -14,57 +14,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-
-    # Getting popular movie
-    popular_movies = get_movies('popular')
-    upcoming_movie = get_movies('upcoming')
-    now_showing_movie = get_movies('now_playing')
-
-    title = 'Home - Welcome to The best Movie Review Website Online'
-
-    search_movie = request.args.get('movie_query')
-
-    if search_movie:
-        return redirect(url_for('search',movie_name=search_movie))
-    else:
-        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
-
-
-@app.route('/movie/<int:id>')
-def movie(id):
-
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    movie = get_movie(id)
-    title = f'{movie.title}'
-    reviews = Review.get_reviews(movie.id)
-
-    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
-
-
-@app.route('/search/<movie_name>')
-def search(movie_name):
-    '''
-    View function to display the search results
-    '''
-    movie_name_list = movie_name.split(" ")
-    movie_name_format = "+".join(movie_name_list)
-    searched_movies = search_movie(movie_name_format)
-    title = f'search results for {movie_name}'
-    return render_template('search.html',movies = searched_movies)
-
-@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
-def new_pitch(id):
-    form = PitchForm()
-    # movie = get_movie(id)
-
-    if form.validate_on_submit():
-        category= form.category.data
-        pitch= form.pitch.data
-        new_pitch= Review(pitch.id,category,pitch)
-        new_pitch.save_review()
-        return redirect(url_for('movie',id = movie.id ))
-
-    title = f'{movie.title} review'
-    return render_template('new_review.html',title = title, review_form=form, movie=movie)
+    title='YouPitch'
+    return render_template('index.html', title = title)
